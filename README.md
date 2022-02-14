18cd2a911a974a5092713d3187cdea1c48bd966f
dylanakiralee <dylanakiralee@gmail.com>
2022-02-14 13:59:21 -0800

    Implement reservation system

--- README.md
+++ README.md
@@ -1,14 +1,21 @@
-# walmart-coding-challenge
+# Walmart Coding Challenge
 
 Movie Theater Seating Challenge (2020) for Walmart Labs.
 
 ### Assumptions
 
-- After doing some research I found that the most desirable seats were in the center of the rows that are 2/3 away from the screen
+- After doing some research I found that the most desirable seats were in the center of the rows that are 1/2 to 2/3 away from the screen. Some articles said only that 2/3 away was most desirable.
+    - As a result, I gave 2/3 the highest desirability, followed by all the rows down to 1/2, and then alternating higher and lower until I ran out of rows.
 - I gave priority to the first reservations in the input file, evaluating each request one by one.
+- I assumed that the safety restrictions don't apply within parties, so all members of a party can sit next to one another.
 
 ### Instructions for Executing
 
 1. Navigate to the directory where theater.py is located.
 2. Run the command `python theater.py input.txt` where input.txt contains the reservation requests.
 3. Check the file output.txt for the seating assignments.
+
+### Improvements
+
+- Create a more robust algorithm that considers multiple requests when making assignments, instead of just the current one.
+- 
--- theater.py
+++ theater.py
@@ -1,7 +1,14 @@
 import sys
+from collections import defaultdict
 
+# Converts row and column value to movie theater seat designation
+def indToSeat(coord):
+    r = chr(ord('A') + coord[0])
+    c = str(coord[1] + 1)
+    return r + c
+
+# Gets input from file and places it in a 2D array
 def parseFile(filename):
-    print(filename)
     with open(filename, 'r') as f:
         lines = f.readlines()
         requests = [['' for _ in range(2)] for _ in range(len(lines))]
@@ -9,8 +16,46 @@ def parseFile(filename):
             requests[i][0], requests[i][1] = line.split()
     return requests
 
+# Writes output to file given assignment dictionary
 def writeToOutput(assignments):
-    pass
+    out = list()
+    for key, values in assignments.items():
+        seats = ','.join([indToSeat(coord) for coord in values])
+        out.append(key + " " + seats)
+
+    with open("output.txt", "w") as f:
+        f.writelines("\n".join(out))
+
+# Sets assigned seats and three seat buffer to True
+def setSeats(row, i, j):
+    length = len(row)
+    for k in range(i - 3, j + 3):
+        if i >= 0 and i < length:
+            row[k] = True
+    return row
+
+# Checks and assigns party to seats in a particular row
+# Returns a changed theater and assignments variables if successful, None if not
+def checkRow(theater, rowNum, request, assignments):
+    i = j = 0
+    row = theater[rowNum]
+    numPeople = int(request[1])
+    length, count = len(row), numPeople
+    while j < length:
+        while i < length and row[i] == True: i += 1 # Sets left side of sliding window
+        j = i
+        while j < length and count > 0 and row[j] == False: # Attempts to find the requested number of seats
+            j += 1
+            count -= 1
+        if count == 0:
+            row = setSeats(row, i, j)
+            theater[rowNum] = row
+            for ind in range(i, j):
+                assignments[request[0]].append((rowNum, ind))
+            return (theater, assignments)
+        count = numPeople
+    return None
+
 
 def assign():
     rows, cols = 20, 10
@@ -22,13 +67,30 @@ def assign():
     
     theater = [[False for _ in range(cols)] for _ in range(rows)]
 
-    preferredRow = rows * 2 // 3 - 1
+    # Order in which the rows are checked for seats
+    priority = [12, 10, 14, 8, 16, 6, 18, 4, 2, 0] # Starts at 2/3 away from screen, goes to 1/2, and then alternates above and below
+    assignments = defaultdict(list)
+    accomodateAll = True
     for request in requests:
-        numPeople = request[1]
-
+        assigned = False
+        for p in priority:
+            if theater[p][0] == True:
+                continue
+            else:
+                assignment = checkRow(theater, p, request, assignments)
+                if assignment is not None:
+                    theater, assignments = assignment
+                    assigned = True
+                    break
+        
+        if not assigned:
+            raise Exception("Error: Could not accomodate all requests\n")
+    
+    writeToOutput(assignments)
 
 def main():
     assign()
+    
 
 if __name__ == "__main__":
     main()
